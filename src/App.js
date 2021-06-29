a9d54c2c18d90497ab7d3d7ed8c89187e2caf364
Adrian Belmonte sepulveda <recseproductions@gmail.com>
2021-06-29 17:00:34 +0200

    header hecho

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Particles from 'react-particles-js';
 
 function App() {
   return (
-    <h2>Portfolio</h2>
+    <>
+        <Particles
+        className="particles-canvas"
+          params={{
+            particles: {
+              number: {
+                value: 30,
+                density: {
+                  enable: true,
+                  value_area: 900
+                }
+              },
+            shape: {
+              type: "square",
+              stroke: {
+                width: 6,
+                color: "#f9ab00"
+              }
+            }
+          }
+          }}
+      />
+      <Navbar />
+      <Header />
+    </>
   );
 }
 
